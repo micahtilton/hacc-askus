acdb245bbc0dca512e025754e356495b4f9615d3
Kaylee Agorilla <kayleeag@hawaii.edu>
2023-10-25 10:10:11 -1000

    Import bootstrap and stylesheet.

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { App } from '/imports/ui/layout/App';
+import "bootstrap/dist/css/bootstrap.css"
+import "./main.css"
 
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
