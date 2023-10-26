ff695ec5d686fd415ee261066f4f847716c47f2d
micahtilton <72249867+micahtilton@users.noreply.github.com>
2023-10-25 14:11:20 -1000

    added meteor methods

--- app/server/main.js
+++ app/server/main.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 
+Meteor.methods({
+  async askHoku(question) {
+    await new Promise(r => setTimeout(r, 2000));
+    return "hi, i am your assistant"
+  }
+})
+
 Meteor.startup(async () => {
 
 });
