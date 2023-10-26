56d64054a818e3d010cc7bd9bf2622fd3c5f119e
micahtilton <72249867+micahtilton@users.noreply.github.com>
2023-10-25 15:12:22 -1000

    removed eslint

--- app/server/main.js
+++ app/server/main.js
@@ -3,9 +3,9 @@ import { Meteor } from 'meteor/meteor';
 Meteor.methods({
   async askHoku(question) {
     await new Promise(r => setTimeout(r, 2000));
-    return "hi, i am your assistant"
-  }
-})
+    return 'hi, i am your assistant';
+  },
+});
 
 Meteor.startup(async () => {
 
