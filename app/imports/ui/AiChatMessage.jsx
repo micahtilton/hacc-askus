1bfe8a50068d58fec5b450e37dfad00854285466
micahtilton <72249867+micahtilton@users.noreply.github.com>
2023-10-25 21:46:07 -1000

    commit

--- app/imports/ui/AiChatMessage.jsx
+++ app/imports/ui/AiChatMessage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AiChatMessage = ({ text, loading=false }) => (
-  <div className="align-self-start d-flex justify-content-start p-2 m-2 bg-primary rounded-3 mw-75 text-white">
+  <div className="align-self-start d-flex justify-content-start p-2 m-2 bg-primary rounded-3 mw-75 text-white text-break">
     { loading ?
       <div className="d-flex">
         <span className="dot delay-1"/>
