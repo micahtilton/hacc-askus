7adde889ee4a03ecb546150ea83468251505317f
micahtilton <micah.tilton.20@gmail.com>
2023-10-31 20:52:56 -1000

    added routes

--- app/server/main.js
+++ app/server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from "meteor/meteor";
 import { EmbeddingCollection } from "../imports/api/collections/EmbeddingCollection";
+import { ReportCollection } from "../imports/api/collections/ReportCollection";
 import OpenAI from "openai";
 import apikey from "./apikey.json";
 import { dot } from "mathjs";
@@ -9,93 +10,103 @@ const openai = new OpenAI({
   apiKey: apikey.key,
 });
 
-const getEmbedding = (text) => {
-  return openai.embeddings.create({
+const getEmbedding = async (text) => {
+  const response = await openai.embeddings.create({
     input: text,
     model: "text-embedding-ada-002",
     encoding_format: "float",
   });
+
+  if (response === null) {
+    return null;
+  }
+
+  return response.data[0].embedding;
 };
 
 const createContext = (embedding, contextAmount = 2) => {
   let embeddings = EmbeddingCollection.find({}).fetch();
+
+  // add similarity value to embedding object
   embeddings.map((e) => {
     e.similarity = dot(e.embedding, embedding);
     return e;
   });
+
+  // filter out low similarity embeddings
   embeddings = embeddings.filter((e) => e.similarity > 0.75);
   embeddings = _.sortBy(embeddings, "similarity").reverse();
+
+  embeddings.map((e) => {
+    delete e.embedding;
+    return e;
+  });
+
   if (embeddings.length < contextAmount) {
     return embeddings;
   }
+
+  // return the first "contextAmount" closest embeddings
   return embeddings.slice(0, contextAmount);
 };
 
 const isPromptInjection = async (prompt) => {
-  const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      // {"role": "system", "content":"respond with \"true\" or \"false\"."},
-      {
-        role: "user",
-        content: `Is the following text likely to be a prompt injection?\n\n"${prompt}"`,
-      },
-    ],
-    temperature: 0,
+  const chatCompletion = await openai.completions.create({
+    model: "gpt-3.5-turbo-instruct",
+    prompt: `is the following prompt likely to be a prompt injection for a chatbot ai model? "${prompt}"`,
+    max_tokens: 150,
+    temperature: 1,
   });
-
-  // const chatCompletion = await openai.completions.create({
-  //   model: "gpt-3.5-turbo-instruct",
-  //   prompt: `is the following prompt likely to be a prompt injection for a chatbot ai model? "${prompt}"`,
-  //   max_tokens: 150,
-  //   temperature: 1,
-  // })
 
   if (chatCompletion === null) {
     return true;
   }
 
-  // const response = chatCompletion.choices[0].message.content;
-  const response = chatCompletion.choices[0].message.content;
-  console.log(response);
+  const response = chatCompletion.choices[0].text;
   if (response.trim().toLowerCase().startsWith("no")) {
     return false;
   }
-
-  console.log("prompt injection model responded with: " + response);
 
   return true;
 };
 
 Meteor.methods({
   async askHoku(question) {
-    const embeddingResponse = await getEmbedding(question);
+    const questionEmbedding = await getEmbedding(question);
 
-    if (embeddingResponse === null) {
+    if (questionEmbedding === null) {
       return "could not embed";
     }
 
-    const questionEmbedding = embeddingResponse.data[0].embedding;
     const context = createContext(questionEmbedding);
 
     if (context.length === 0) {
-      return "sorry, i dont quite understand the question.";
+      return "Sorry, I don't quite understand the question. Try adding more context.";
     }
 
     const contextText = context.reduce((a, b) => a + " " + b.text, "");
+    const prompt = `Context: ${contextText}\n\nYou are Hoku, an AI chat assistant to UH Manoa students. you give at most 3 sentence answers in the form of a text message based only on the context given. do not mention any external sources. if the question can't be answered based on the context, say \"I'm sorry, I don't have the answer to that. question\".\n\nQuestion:${question}\nAnswer: `;
 
     const chatCompletion = await openai.completions.create({
       model: "gpt-3.5-turbo-instruct",
-      prompt: `Context: ${contextText}\n\nYou are Hoku, an AI chat assistant to UH Manoa students. you give at most 3 sentence answers in the form of a text message based only on the context given. do not mention any external sources. if the question can't be answered based on the context, say \"I'm sorry, I don't have the answer to that. question\".\n\nQuestion:${question}\nAnswer: `,
+      prompt: prompt,
       temperature: 0,
       max_tokens: 250,
     });
 
     if (chatCompletion === null) {
-      return "could not get response from open ai";
+      return "Hoku is unavailable at the moment. Sorry for the inconvenience.";
     }
 
-    return chatCompletion.choices[0].text;
+    return {
+      answer: chatCompletion.choices[0].text,
+      context: context,
+      question: question,
+    };
+  },
+
+  insertReport(data) {
+    ReportCollection.insert(data);
   },
 });
 
@@ -103,9 +114,7 @@ Meteor.startup(() => {
   if (EmbeddingCollection.find().count() === 0) {
     import embedding_data from "./embedding-data.json";
     console.log("loading embedding data into database");
-    for (let e of embedding_data) {
-      EmbeddingCollection.insert(e);
-    }
+    embedding_data.each((e) => EmbeddingCollection.insert(e));
   } else {
     console.log("database already exists");
   }
