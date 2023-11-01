2fbec5dfa8e7600cb3d0baf85b89b3806f86773d
Kaylee Agorilla <kayleeag@hawaii.edu>
2023-11-01 10:11:56 -1000

    Cleaned up code for Home and HelpDesk

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -3,16 +3,17 @@ import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Home = () => (
-  <Container className='py-4 '>
+  <Container className="py-4 ">
     <Row className="align-middle text-center">
       <Col lg={8} className="">
         <h1>Meet Hoku!</h1>
         <p>Ask Us is a knowledge base of frequently asked questions (FAQs) related to information technology. Enter keywords related to your question in the search box, then click on "Ask Us". FAQs that match the keywords will be displayed.
-          Click on the match that best answers your question. Click on "Ask Us" again to return to the list of matches, or enter new keywords for a new search.</p>
+          Click on the match that best answers your question. Click on "Ask Us" again to return to the list of matches, or enter new keywords for a new search.
+        </p>
         <Button>Ask a Question</Button>
       </Col>
       <Col className="align-middle">
-        <Image src="images/Hoku.png" id="hoku" fluid/>
+        <Image src="images/Hoku.png" id="hoku" fluid />
       </Col>
     </Row>
   </Container>
