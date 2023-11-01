d673a1cfe1a5b83aa47adf95e107592a40f20df3
micahtilton <micah.tilton.20@gmail.com>
2023-11-01 03:21:18 -1000

    updated client

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Meteor } from 'meteor/meteor';
-import 'bootstrap/dist/css/bootstrap.css';
-import './main.css';
-import './chat.css'
-import { App } from '../imports/ui/layout/App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Meteor } from "meteor/meteor";
+import "bootstrap/dist/css/bootstrap.css";
+import "./main.css";
+import { App } from "../imports/ui/layout/App";
 
 Meteor.startup(() => {
-  const container = document.getElementById('react-target');
+  const container = document.getElementById("react-target");
   const root = createRoot(container);
   root.render(<App />);
 });
