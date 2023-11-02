b1fa83d996a5a2964b38fc73b90fe72127c105e5
micahtilton <micah.tilton.20@gmail.com>
2023-11-02 00:59:11 -1000

    moved collections

--- app/imports/api/ReportCollection.js
+++ app/imports/api/ReportCollection.js
@@ -1,27 +1,28 @@
 import { Mongo } from "meteor/mongo";
 import SimpleSchema from "simpl-schema";
-import { EmbeddingSchema } from "./EmbeddingCollection";
 
 const ReportCollection = new Mongo.Collection("reports");
 
-const EmbeddingSchemaExtended = EmbeddingSchema.clone()
-  .extend({
-    similarity: { type: Number },
-  })
-  .omit("embedding");
+const ContextSchema = new SimpleSchema({
+  source: { type: String },
+  similarity: { type: Number },
+  text: { type: String },
+});
 
 const ReportSchema = new SimpleSchema({
+  resolved: { type: Boolean },
   context: { type: Array },
   "context.$": {
-    type: EmbeddingSchemaExtended,
+    type: ContextSchema,
   },
   question: { type: String },
   answer: { type: String },
   categories: { type: Array },
   "categories.$": { type: String },
   comment: { type: String },
+  date: { type: Date },
 });
 
 ReportCollection.attachSchema(ReportSchema);
 
-export { ReportCollection, ReportSchema };
+export { ReportCollection, ReportSchema, getReports };
