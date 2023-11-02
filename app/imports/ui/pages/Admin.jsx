46ed30dea51bc5ffdf50a3f8ff87a0d59e72ef22
micahtilton <micah.tilton.20@gmail.com>
2023-11-02 01:00:34 -1000

    added admin and reports page

--- app/imports/ui/pages/Admin.jsx
+++ app/imports/ui/pages/Admin.jsx
@@ -1,1 +1,10 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+
+const Admin = () => (
+  <Container>
+    <h1>Admin</h1>
+  </Container>
+);
+
+export default Admin;
---
+++ app/imports/ui/pages/ReportsPage.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import ReportView from "../components/ReportView";
+import { Col, Container, Row } from "react-bootstrap";
+import { ReportCollection } from "../../api/ReportCollection";
+
+const ReportsPage = () => {
+  const reports = useTracker(() => ReportCollection.find({}).fetch());
+
+  return (
+    <Container fluid>
+      <Col className={""}>
+        <Row className={"d-none d-md-flex"}>
+          <Col md={1} className={"fw-bold"}>
+            Comment
+          </Col>
+          <Col md={2} className={"fw-bold"}>
+            Question
+          </Col>
+          <Col md={4} className={"fw-bold"}>
+            Answer
+          </Col>
+          <Col md={2} className={"fw-bold"}>
+            Tags
+          </Col>
+          <Col md={2} className={"fw-bold"}>
+            Sources
+          </Col>
+        </Row>
+        {reports.map((report, index) => (
+          <ReportView key={report._id} index={index} report={report} />
+        ))}
+      </Col>
+      <Col md={1}></Col>
+    </Container>
+  );
+};
+
+export default ReportsPage;
