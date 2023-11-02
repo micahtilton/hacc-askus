76a50387aa37dab6c3d83ef91164e3779ecf4f7e
micahtilton <micah.tilton.20@gmail.com>
2023-11-02 01:00:44 -1000

    updated app

--- app/imports/ui/layout/App.jsx
+++ app/imports/ui/layout/App.jsx
@@ -5,6 +5,15 @@ import Footer from "../components/Footer";
 import ChatBot from "../ChatBot";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HelpDesk from "../pages/HelpDesk";
+import Admin from "../pages/Admin";
+import { ReportCollection } from "../../api/ReportCollection";
+import ReportsPage from "../pages/ReportsPage";
+
+const PageNotFound = () => (
+  <div className={"d-flex justify-content-center"}>
+    <h1 className={"p-5"}>Error 404: Page Not Found</h1>
+  </div>
+);
 
 export const App = () => (
   <Router>
@@ -14,6 +23,8 @@ export const App = () => (
         <Route exact path="/" element={<Home />} />
         <Route exact path="/help" element={<HelpDesk />} />
         <Route exact path="/admin" element={<Admin />} />
+        <Route exact path="/admin/report" element={<ReportsPage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <ChatBot />
       <Footer />
