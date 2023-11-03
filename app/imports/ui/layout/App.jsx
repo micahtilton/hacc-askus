7d73774581b4eb7f138b9e012f84f4b2960de745
micahtilton <micah.tilton.20@gmail.com>
2023-11-02 19:25:00 -1000

    updated app

--- app/imports/ui/layout/App.jsx
+++ app/imports/ui/layout/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Home from "../pages/Home";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import ChatBot from "../ChatBot";
+import ChatBot from "../pages/chatbot/ChatBot";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HelpDesk from "../pages/HelpDesk";
 import Admin from "../pages/Admin";
-import { ReportCollection } from "../../api/ReportCollection";
-import ReportsPage from "../pages/ReportsPage";
+import ReportsPage from "../pages/report/ReportsPage";
+import ManageFAQPage from "../pages/faq/ManageFAQPage";
 
 const PageNotFound = () => (
   <div className={"d-flex justify-content-center"}>
@@ -24,6 +24,7 @@ export const App = () => (
         <Route exact path="/help" element={<HelpDesk />} />
         <Route exact path="/admin" element={<Admin />} />
         <Route exact path="/admin/report" element={<ReportsPage />} />
+        <Route exact path="/admin/faq" element={<ManageFAQPage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
       <ChatBot />
