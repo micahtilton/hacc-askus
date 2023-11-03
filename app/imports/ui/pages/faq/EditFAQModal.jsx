66995e663fd57b919e290671e624717860e5261f
micahtilton <micah.tilton.20@gmail.com>
2023-11-02 19:26:22 -1000

    moved faq files

---
+++ app/imports/ui/pages/faq/EditFAQModal.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { ResolvedCollection } from "../../../api/ResolvedCollection";
+import { Button, Form, Modal } from "react-bootstrap";
+
+const EditFAQModal = ({ faq }) => {
+  const [questionText, setQuestionText] = useState("");
+  const [answerText, setAnswerText] = useState(faq.text);
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setQuestionText(faq.question);
+    setAnswerText(faq.text);
+    setShow(true);
+  };
+
+  const handleSubmit = () => {
+    Meteor.call("editFAQ", faq._id, questionText, answerText, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    handleClose();
+  };
+
+  const handleDelete = () => {
+    ResolvedCollection.remove(faq._id);
+    handleClose();
+  };
+
+  return (
+    <>
+      <div>
+        <Button onClick={handleShow}>Edit</Button>
+      </div>
+
+      <Modal centered show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Faq</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className={"overflow-y-auto"}>
+          <Form
+            id={"edit-faq-form"}
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <Form.Group className="mb-3">
+              <Form.Label>Question</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Question"
+                value={questionText}
+                onChange={(e) => {
+                  setQuestionText(e.target.value);
+                }}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Answer</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Answer"
+                value={answerText}
+                onChange={(e) => {
+                  setAnswerText(e.target.value);
+                }}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleDelete}>
+            Delete
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button type="submit" form="edit-faq-form" variant="primary">
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default EditFAQModal;
