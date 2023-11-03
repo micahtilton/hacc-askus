fb4e1175be0273978a9ac28942ff4062bb17b834
micahtilton <micah.tilton.20@gmail.com>
2023-11-02 19:27:09 -1000

    updated main

--- app/server/main.js
+++ app/server/main.js
@@ -5,50 +5,11 @@ import OpenAI from "openai";
 import apikey from "./apikey.json";
 import { dot } from "mathjs";
 import { _ } from "underscore";
+import { ResolvedCollection } from "../imports/api/ResolvedCollection";
 
 const openai = new OpenAI({
   apiKey: apikey.key,
 });
-
-const getEmbedding = async (text) => {
-  const response = await openai.embeddings.create({
-    input: text,
-    model: "text-embedding-ada-002",
-    encoding_format: "float",
-  });
-
-  if (response === null) {
-    return null;
-  }
-
-  return response.data[0].embedding;
-};
-
-const createContext = (embedding, contextAmount = 2) => {
-  let embeddings = EmbeddingCollection.find({}).fetch();
-
-  // add similarity value to embedding object
-  embeddings.map((e) => {
-    e.similarity = dot(e.embedding, embedding);
-    return e;
-  });
-
-  // filter out low similarity embeddings
-  embeddings = embeddings.filter((e) => e.similarity > 0.75);
-  embeddings = _.sortBy(embeddings, "similarity").reverse();
-
-  embeddings.map((e) => {
-    delete e.embedding;
-    return e;
-  });
-
-  if (embeddings.length < contextAmount) {
-    return embeddings;
-  }
-
-  // return the first "contextAmount" closest embeddings
-  return embeddings.slice(0, contextAmount);
-};
 
 const isPromptInjection = async (prompt) => {
   const chatCompletion = await openai.completions.create({
@@ -70,6 +31,86 @@ const isPromptInjection = async (prompt) => {
   return true;
 };
 
+const editFAQ = async (id, question, answer) => {
+  ResolvedCollection.remove(id);
+  await addFAQ(question, answer);
+};
+
+const addFAQ = async (question, answer) => {
+  const embedding = await getEmbedding(question);
+
+  if (embedding === null) return false;
+
+  const resolveInfo = {
+    source: "ITS Admin",
+    embedding: embedding,
+    text: answer,
+    question: question,
+  };
+
+  ResolvedCollection.insert(resolveInfo);
+};
+
+const getEmbedding = async (text) => {
+  const response = await openai.embeddings.create({
+    input: text,
+    model: "text-embedding-ada-002",
+    encoding_format: "float",
+  });
+
+  if (response === null) {
+    return null;
+  }
+
+  return response.data[0].embedding;
+};
+
+const createContextFrom = (
+  collection,
+  embedding,
+  contextAmount = 2,
+  similarityThreshold = 1,
+) => {
+  let embeddings = collection.find({}).fetch();
+
+  // add similarity value to embedding object
+  embeddings.map((e) => {
+    e.similarity = dot(e.embedding, embedding);
+    return e;
+  });
+
+  // filter out low similarity embeddings
+  embeddings = embeddings.filter((e) => e.similarity > similarityThreshold);
+  embeddings = _.sortBy(embeddings, "similarity").reverse();
+
+  embeddings.map((e) => {
+    delete e.embedding;
+    return e;
+  });
+
+  if (embeddings.length < contextAmount) {
+    return embeddings;
+  }
+
+  // return the first "contextAmount" closest embeddings
+  return embeddings.slice(0, contextAmount);
+};
+
+const createContext = (embedding, contextAmount = 2) => {
+  const resolvedContext = createContextFrom(
+    ResolvedCollection,
+    embedding,
+    1,
+    0.9,
+  );
+
+  if (resolvedContext.length === 1) {
+    return resolvedContext;
+  }
+
+  return createContextFrom(EmbeddingCollection, embedding, contextAmount, 0.75);
+};
+
 Meteor.methods({
   async askHoku(question) {
     const questionEmbedding = await getEmbedding(question);
@@ -79,13 +120,13 @@ Meteor.methods({
     }
 
     const context = createContext(questionEmbedding);
-
     if (context.length === 0) {
       return "Sorry, I don't quite understand the question. Try adding more context.";
     }
 
     const contextText = context.reduce((a, b) => a + " " + b.text, "");
-    const prompt = `Context: ${contextText}\n\nYou are Hoku, an AI chat assistant to UH Manoa students. you give at most 3 sentence answers in the form of a text message based only on the context given. do not mention any external sources. if the question can't be answered based on the context, say \"I'm sorry, I don't have the answer to that. question\".\n\nQuestion:${question}\nAnswer: `;
+
+    const prompt = `Context: ${contextText}\n\nYou are Hoku, an AI chat assistant to UH Manoa students. you give at most 3 sentence answers in the form of a text message based only on the context above. do not mention any external sources. if the question can't be answered based on the context, say \"I'm sorry, I don't have the answer to that. question\".\n\nQuestion:${question}\nAnswer: `;
 
     const chatCompletion = await openai.completions.create({
       model: "gpt-3.5-turbo-instruct",
@@ -105,13 +146,26 @@ Meteor.methods({
     };
   },
 
-  insertReport(data) {
-    ReportCollection.insert(data);
+  insertReport(report) {
+    ReportCollection.insert(report);
   },
 
-  getReports() {
-    return ReportCollection.find({}).fetch();
+  async resolveReport(id, question, answer) {
+    await addFAQ(question, answer);
+    // ReportCollection.delete(id);
+    return true;
   },
+
+  addFAQ,
+  editFAQ,
+});
+
+Meteor.publish("reports", () => {
+  return ReportCollection.find({});
+});
+
+Meteor.publish("resolved", () => {
+  return ResolvedCollection.find({});
 });
 
 Meteor.startup(() => {
@@ -121,11 +175,17 @@ Meteor.startup(() => {
     console.log("report collection is not empty");
   }
 
+  if (ResolvedCollection.find().count() === 0) {
+    console.log("resolved collection is empty");
+  } else {
+    console.log("resolved collection is not empty");
+  }
+
   if (EmbeddingCollection.find().count() === 0) {
     import embedding_data from "./embedding-data.json";
     console.log("loading embedding data into database");
     embedding_data.forEach((e) => EmbeddingCollection.insert(e));
   } else {
-    console.log("database already exists");
+    console.log("embedding collection is not empty");
   }
 });
