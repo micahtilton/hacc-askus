f62f6b0ecd7fe90c7c6832f25924c391919306fc
micahtilton <micah.tilton.20@gmail.com>
2023-11-04 00:20:12 -1000

    fixed hoku message image

--- app/imports/ui/pages/chatbot/AiChatMessage.jsx
+++ app/imports/ui/pages/chatbot/AiChatMessage.jsx
@@ -31,7 +31,12 @@ const AiChatMessage = ({ context, loading = false }) => {
 
   return (
     <div className={"d-flex align-items-end"}>
-      <Image src={"hoku-pfp.png"} width={40} height={40} className={"mx-2"} />
+      <Image
+        src={"images/hoku-pfp.png"}
+        width={40}
+        height={40}
+        className={"mx-2"}
+      />
       <div className="ai-text-box align-self-start d-flex p-2 justify-content-start bg-primary text-white text-break me-lg-5 me-sm-2">
         {loading ? (
           <ChatLoading />
