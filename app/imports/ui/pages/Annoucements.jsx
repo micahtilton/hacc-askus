88885f4c3d26c6da9a35593b51bc965a0c35cc98
Malisa Lo <malisalo1703@gmail.com>
2023-11-06 03:58:28 -1000

    Create Annoucements.jsx

---
+++ app/imports/ui/pages/Annoucements.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Col, Container } from 'react-bootstrap';
+
+const Announcements = () => (
+  <Container>
+    <Row>
+      <Col>
+        <h1>Announcements</h1>
+
+      </Col>
+      <Col>
+        <h1>Scheduled Maintenance</h1>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Announcements;
