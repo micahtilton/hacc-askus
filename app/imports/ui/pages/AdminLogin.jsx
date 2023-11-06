08dbe0a3f48ae84fa45f457132c2725388371bc8
Kaylee Agorilla <kayleeag@hawaii.edu>
2023-11-05 23:19:51 -1000

    Finished Admin Login U.I.

--- app/imports/ui/pages/AdminLogin.jsx
+++ app/imports/ui/pages/AdminLogin.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
-import { Navigate, redirect } from "react-router-dom";
+import React, { useState } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Button, Col, Container, Form, Image, Row } from 'react-bootstrap';
+import { Navigate } from 'react-router-dom';
+import { EnvelopeAtFill, LockFill } from 'react-bootstrap-icons';
 
 const AdminLogin = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [redirect, setRedirect] = useState(false);
 
   const resetForm = () => {
-    setEmail("");
-    setPassword("");
+    setEmail('');
+    setPassword('');
   };
   const handleSubmit = () => {
     Meteor.loginWithPassword(email, password, (err) => {
@@ -25,44 +27,58 @@ const AdminLogin = () => {
   return redirect ? (
     <Navigate to="/admin/report" />
   ) : (
-    <Container>
-      <Form
-        className={"p-3 m-5 rounded-3 border border-2 bg-white "}
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}
-      >
-        <div>
-          <h2 className={"pb-3"}>Sign In</h2>
-        </div>
-        <Form.Group className={"pb-3"}>
-          <Form.Label className={"mb-0"}>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
+    <Container className="py-5">
+      <Row className="justify-content-center">
+        <Col sm={6}>
+          <Form
+            id="login"
+            className="p-3 m-5 rounded-3 border border-2 bg-white"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
             }}
-          />
-        </Form.Group>
+          >
+            <Col className="text-center">
+              <Image src="/images/hoku-pfp.png" roundedCircle style={{ width: '90px', height: '90px' }} />
+              <h2>Admin Login</h2>
+            </Col>
+            <Form.Group className="pb-3">
+              <Form.Label className="mb-0 mx-sm-3">Email address</Form.Label>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <EnvelopeAtFill className="mx-sm-3 mb-1" size="20" id="envelope" />
+                <Form.Control
+                  id="email"
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </div>
+            </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label className={"mb-0"}>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+            <Form.Group className="pb-3">
+              <Form.Label className="mb-0 mx-sm-3">Password</Form.Label>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <LockFill className="mx-sm-3 mb-1" size="20" id="lock" />
+                <Form.Control
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+              </div>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
     </Container>
   );
 };
