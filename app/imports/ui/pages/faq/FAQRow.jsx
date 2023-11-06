0848eb4ddc6792ded8270cf00f828a76fc0eab8f
micahtilton <micah.tilton.20@gmail.com>
2023-11-06 03:10:56 -1000

    slight ui changes

--- app/imports/ui/pages/faq/FAQRow.jsx
+++ app/imports/ui/pages/faq/FAQRow.jsx
@@ -18,7 +18,7 @@ const FAQRow = ({ faq, index }) => {
         <div className={"fw-bold"}>Source: </div>
         <div>{faq.source}</div>
       </Col>
-      <Col className={"d-flex justify-content-end"}>
+      <Col xs={12} md={1} className={"d-flex justify-content-center pt-2"}>
         <EditFAQModal faq={faq} />
       </Col>
     </Row>
