fa3612ac38fa2275c9e5ab6d976888536f71b973
micahtilton <72249867+micahtilton@users.noreply.github.com>
2023-11-12 04:13:15 -1000

    prompt injection testing

---
+++ api/ai/prompt_injection.py
@@ -0,0 +1,27 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+
+# Load data
+test = pd.read_parquet("./data/test.parquet")
+train = pd.read_parquet("./data/train.parquet")
+
+# Parse data
+X_train, y_train = train["text"], train["label"]
+X_test, y_test = test["text"], test["label"]
+
+# Create TF-IDF Vectorizer
+vectorizer = TfidfVectorizer()
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+
+# Create and fit naive bayes prompt injection classifier
+classifier = MultinomialNB()
+classifier.fit(X_train_tfidf, y_train)
+
+print("Created naive bayes prompt injection classifier")
+
+
+def is_prompt_injection(text):
+    text_tf_idf = vectorizer.transform([text])
+    return True if classifier.predict(text_tf_idf)[0] else False
